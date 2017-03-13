548cb18a6db20aaa920da2e7f5556a78ff6f84ab
Lee Forest <lee8oi@gmail.com>
2017-03-12 20:48:39 -0400

    Further simplification of code.

--- main.go
+++ main.go
@@ -20,26 +20,22 @@ var pubdir = flag.String("pubdir", "public", "path to public directory")
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", indexServer)
-	http.Handle("/pub/", http.StripPrefix("/pub/", http.FileServer(http.Dir(*pubdir))))
-	go log.Fatal(http.ListenAndServeTLS(":"+*httpsPort, *certPem, *keyPem, nil))
-	log.Fatal(http.ListenAndServe(":"+*httpPort, nil))
-}
-
-// indexServer serves the index.html.
-func indexServer(w http.ResponseWriter, r *http.Request) {
-	redirect(w, r)
-	http.ServeFile(w, r, *pubdir+"/index.html")
-}
-
-// redirect will reroute to https url if TLS not in use.
-func redirect(w http.ResponseWriter, r *http.Request) {
-	if r.TLS == nil {
-		http.Redirect(w, r, func() string {
-			if *httpsPort == "443" {
-				return "https://" + *domain
-			}
-			return "https://" + *domain + ":" + *httpsPort
-		}(), 301)
+	go func() {
+		err := http.ListenAndServeTLS(":"+*httpsPort, *certPem, *keyPem, http.FileServer(http.Dir(*pubdir)))
+		if err != nil {
+			log.Fatal("ListenAndServeTLS:", err)
+		}
+	}()
+	err := http.ListenAndServe(":"+*httpPort, http.RedirectHandler(httpsUrl(), 301))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+// httpsUrl returns the servers TLS url.
+func httpsUrl() string {
+	if *httpsPort == "443" {
+		return "https://" + *domain
+	}
+	return "https://" + *domain + ":" + *httpsPort
 }
